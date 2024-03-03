7c1d7a5cfd4fd84700b8b65610a08f62ae3fd645
Kysel95 <kysel95@gmail.com>
2024-03-03 22:18:01 +0100

    Updated with Login Register and Profile + functions

---
+++ TrainingApp/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  TrainingApp
+//
+//  Created by kysel95 on 03/03/2024.
+//
+
+import SwiftUI
+
+struct User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User()
+}
