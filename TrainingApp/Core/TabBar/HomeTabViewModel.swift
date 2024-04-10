0f004ff4528b78ce3bc28cfb9232adfc07cb850f
Kysel95 <kysel95@gmail.com>
2024-04-10 23:45:47 +0200

    added TabBar for testing with only One page

---
+++ TrainingApp/Core/TabBar/HomeTabViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTabViewModel.swift
+//  TrainingApp
+//
+//  Created by kysel95 on 10/04/2024.
+//
+
+import Foundation
---
+++ TrainingApp/Utilis/ViewModifiers/BackgroundModifier.swift
@@ -0,0 +1,8 @@
+//
+//  BackgroundModifier.swift
+//  TrainingApp
+//
+//  Created by kysel95 on 07/04/2024.
+//
+
+import Foundation
